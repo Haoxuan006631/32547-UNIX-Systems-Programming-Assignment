bcd00b79aecf1ce3f1b44901beb733d950dbe49c
JiayiChristina <126847199+Haoxuan006631@users.noreply.github.com>
2025-10-26 18:41:01 +1100

    finish function option-c option -v

--- LLMinfo.py
+++ LLMinfo.py
@@ -36,6 +36,35 @@ def optopm_r(filename):
     if count == 0:
         print("No remote models in this file")
 
+def option_c(filename, company):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        if not lines:
+            print("No models in this file")
+            return
+    with open(filename, 'r') as file:
+        count = 0
+        for line in file:
+            line = line.strip()          
+            parts = line.split(',')
+            #print(parts)   
+            if parts[1].lower() == company.lower():
+                if count == 0:
+                    print(f"Models from {company}:")
+                print(parts[0])
+                count += 1
+            else:
+                continue
+    if count == 0:
+        print(f"No models from {company} in this file")
+def option_s(filename, size):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        if not lines:
+            print("No models in this file")
+            return
+def option_v(filename):
+    print("Name:Haoxuan Jin\nStudent ID: 25167901\nCourse: 32547-UNIX Systems Programming\nfinished date: 2025-10-26\ngithub link:https://github.com/Haoxuan006631/32547-UNIX-Systems-Programming-Assignment.git")
 
 def main():
     input_list = sys.argv[1:]
@@ -61,7 +90,20 @@ def main():
         sys.exit(1)
     
     #option_a(filename)  
-    #optopm_r(filename)  
-
+    #optopm_r(filename) 
+    if options == "-a":
+        option_a(filename)  
+    elif options == "-r":
+        optopm_r(filename) 
+    elif options == "-c":
+        company = input_list[-2]
+        #print(company)
+        option_c(filename, company)
+    elif options == "-s":
+        size = input_list[-2]
+        #print(size)
+        option_s(filename, size)
+    elif options == "-v":
+        option_v(filename)
 if __name__ == "__main__":
     main()
