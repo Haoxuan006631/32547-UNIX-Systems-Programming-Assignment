b0cc0a666ac71315508dd20d3db3789c850bba13
JiayiChristina <126847199+Haoxuan006631@users.noreply.github.com>
2025-10-26 17:38:58 +1100

    Complete the condition judgment.

---
+++ LLMinfo.py
@@ -0,0 +1,35 @@
+import sys
+import os
+def option_a(filename):
+    with open(filename, 'r') as file:
+        data = file.read()
+        print(data)
+
+def main():
+    input_list = sys.argv[1:]
+    #test input
+    #print(input_list)
+    #print(len(input_list))
+    if len(input_list) < 2:
+        print("it misses the argument file")
+        sys.exit(1)
+    else:
+        filename  = input_list[-1].lower()
+        options = input_list[0].lower()
+        #test fileneme and options correct
+        #print(options, filename)
+    if options == "-s" and len(input_list) != 3:
+        print("it misses the size argument")
+        sys.exit(1)
+    elif options == "-c" and len(input_list) != 3:
+        print("it misses the company argument")
+        sys.exit(1)
+    elif options not in["-a", "-r", "-c", "-s", "-v"]:
+        print("this option doesn't exist")
+        sys.exit(1)
+    
+    option_a(filename)  
+
+
+if __name__ == "__main__":
+    main()
